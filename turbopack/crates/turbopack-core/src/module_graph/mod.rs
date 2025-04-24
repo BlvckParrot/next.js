5a08cc0e3ec7760c3699c38b1a748c274d6d663c
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-04-24 10:47:46 -0700

    fix graph?

--- turbopack/crates/turbopack-core/src/module_graph/mod.rs
+++ turbopack/crates/turbopack-core/src/module_graph/mod.rs
@@ -287,7 +287,11 @@ impl SingleModuleGraph {
                             graph.add_edge(parent_idx, current_idx, RefData { export, ..ref_data });
                         }
                     }
-                    SingleModuleGraphBuilderNode::VisitedModule { module, idx } => {
+                    SingleModuleGraphBuilderNode::VisitedModule {
+                        module,
+                        idx,
+                        export,
+                    } => {
                         // Find the current node, if it was already added
                         let current_idx = if let Some(current_idx) = modules.get(&module) {
                             *current_idx
@@ -298,8 +302,16 @@ impl SingleModuleGraph {
                             idx
                         };
                         // Add the edge
-                        if let Some((parent_idx, chunking_type)) = parent_edge {
-                            graph.add_edge(parent_idx, current_idx, chunking_type);
+                        if let Some((parent_idx, data)) = parent_edge {
+                            graph.add_edge(
+                                parent_idx,
+                                current_idx,
+                                RefData {
+                                    export,
+                                    ..data.clone()
+                                },
+                            );
+                            graph.add_edge(parent_idx, current_idx, data);
                         }
                     }
                     SingleModuleGraphBuilderNode::ChunkableReference {
@@ -1276,6 +1288,7 @@ enum SingleModuleGraphBuilderNode {
     VisitedModule {
         module: ResolvedVc<Box<dyn Module>>,
         idx: GraphNodeIndex,
+        export: ExportUsage,
     },
     /// Issues to be added to the parent Module node
     #[allow(dead_code)]
@@ -1312,8 +1325,16 @@ impl SingleModuleGraphBuilderNode {
             },
         })
     }
-    fn new_visited_module(module: ResolvedVc<Box<dyn Module>>, idx: GraphNodeIndex) -> Self {
-        Self::VisitedModule { module, idx }
+    fn new_visited_module(
+        module: ResolvedVc<Box<dyn Module>>,
+        idx: GraphNodeIndex,
+        export: ExportUsage,
+    ) -> Self {
+        Self::VisitedModule {
+            module,
+            idx,
+            export,
+        }
     }
 }
 struct SingleModuleGraphBuilderEdge {
@@ -1387,7 +1408,9 @@ impl Visit<SingleModuleGraphBuilderNode> for SingleModuleGraphBuilder<'_> {
                         .map(async |(ty, export, target)| {
                             let to = if ty == COMMON_CHUNKING_TYPE {
                                 if let Some(idx) = visited_modules.get(&target) {
-                                    SingleModuleGraphBuilderNode::new_visited_module(target, *idx)
+                                    SingleModuleGraphBuilderNode::new_visited_module(
+                                        target, *idx, export,
+                                    )
                                 } else {
                                     SingleModuleGraphBuilderNode::new_module(target, export).await?
                                 }
@@ -1413,6 +1436,7 @@ impl Visit<SingleModuleGraphBuilderNode> for SingleModuleGraphBuilder<'_> {
                             SingleModuleGraphBuilderNode::new_visited_module(
                                 chunkable_ref_target,
                                 *idx,
+                                export,
                             )
                         } else {
                             SingleModuleGraphBuilderNode::new_module(chunkable_ref_target, export)
